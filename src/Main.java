3cbf32ddb5a88943e4e9b74fd9da26d9d63d86d5
kacper <kacper.perz1@gmai.com>
2019-09-05 21:55:46 +0200

    continue master

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Zaczynam działanie");
+        System.out.println("Zaczynam działanie, start...");
+
+        Rectangle rec = new Rectangle();
+        rec.a = 10;
+        rec.b = 10;
+
+        System.out.println(rec.getCicrumference());
 
 
-        System.out.println("Kończę działanie");
+        System.out.println("Kończę działanie, koniec...");
     }
 }
