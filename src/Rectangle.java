f7130e247d36aa4bce99031b081c62a9641f4ebc
kacper <kacper.perz1@gmai.com>
2019-09-05 21:51:21 +0200

    init2

---
+++ src/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    public int a;
+    public int b;
+
+    public int getArea() {
+        return 4*a*b;
+    }
+
+    public int getCicrumference() {
+        return 4*(a+b);
+    }
+}
